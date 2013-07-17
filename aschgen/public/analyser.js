3f6b43a4cf8ca6abe5376b7fd51d61bda3a43a2a
Juergen Brandstetter <juergen@tagaprice.org>
2013-07-17 21:05:31 +1200

    calc new tools

--- aschgen/public/analyser.js
+++ aschgen/public/analyser.js
@@ -1,6 +1,6 @@
 window.aschTracks = [];
 window.aschResults = [];
-window.excludeIds = ["72ccc272d29a684a","14f59de248141a77"];
+window.excludeIds = ["72ccc272d29a684a","14f59de248141a77","2031318a5fa4b87e", "6ecf3abea85e3adb"];
 
 /**
 * Get all people  
@@ -51,10 +51,11 @@ function startStats(){
      for(var k = 0; k < window.aschResults.length; k+=1){
        if(window.aschTracks[i].id === window.aschResults[k].userid){
          
+         //console.log(window.aschResults[k].date);
          if(thinking != null){
            window.aschResults[k].thinkTime = window.aschResults[k].date - thinking;
-         }else {
-           window.aschResults[k].thinkTime = 0;
+         }else{
+           thinking=0;
          }
          
          window.aschTracks[i].data.push(window.aschResults[k]);
@@ -66,16 +67,60 @@ function startStats(){
    
    // clean configue slides
    cleanConfigSlides();
+   
    // create average
-  
+   createAverage();
+   
+   //show averages
+   showAverage();
+    
 }
 
 function cleanConfigSlides(){
   
+  var cfiles = [];
+  for(var k = 0; k < window.configSlides.length; k+=1){
+    
+    if(window.configSlides[k].type === "drawAsch"){
+      cfiles.push(window.configSlides[k]);
+    }
+  }
 
 
-
-
+  window.configSlides = cfiles;
 }
+
+function createAverage(){
+  var trackers = window.aschTracks.length;
+  
+  for(var k = 0; k < window.configSlides.length; k+=1){
+    var slideAverage = 0;
+    var incorrect = 0;
+    for(var i = 0; i < trackers; i+=1){
+      slideAverage += window.aschTracks[i].data[k].thinkTime;
+      
+      if(window.aschTracks[i].data[k].result != window.aschTracks[i].data[k].correct){
+        incorrect += 1;
+      }
+    }
+    
+    window.configSlides[k].wrongs = incorrect;
+    window.configSlides[k].average = (slideAverage/trackers);
+  }
+}
+
+function showAverage(){
+  
+  var problems = 0;
+  for(var k = 0; k < window.configSlides.length; k+=1){
+    if( window.configSlides[k].wrongs > 2){
+      problems+=1;
+      console.log(k, window.configSlides[k].wrongs, window.configSlides[k].average);
+    }
+  }
+  
+  console.log("problems: ",problems);
+}
+
 
 
