9c929cb223c3e227920a706d9db43fc975df5ad5
Juergen Brandstetter <juergen@tagaprice.org>
2013-07-23 18:37:19 +1200

    add audio files and added all possible words and line to finalAshconfig.js

---
+++ aschgen/public/finalAshConfig.js
@@ -0,0 +1,453 @@
+window.configUser = {
+	past : 0, //can be 0 = Baseline, 1 = Version 1, 2 = Version 2
+	participle : 0, //can be 0 = Baseline, 1 = Version 1, 2 = Version 2
+	plural : 0, //can be 0 = Baseline, 1 = Version 1, 2 = Version 2
+	multi : true, //show target colors/Yes no
+};
+
+window.configSlides = 
+[
+
+	/*
+	{
+	  	type : "drawCommand",
+	  	v1 : "Hello!!!",
+	  	v2 : "Hello and welcome to the experiment",  	
+	},
+
+	{
+	  	type : "drawCommand",
+	  	v1 : "Visual Experiment",
+	  	v2 : "Instructions and test round",  	
+	},
+
+	{
+		"type": "drawAsch",
+		"user": 0,
+		"v1": 0.9750000000000005,
+		"v2": 0.025,
+		"v3": 0.175,
+		"answer": 0.025,
+		"answernr": 1
+	},
+	{
+		"type": "drawAsch",
+		"user": 1,
+		"v1": 0.9750000000000005,
+		"v2": 0.025,
+		"v3": 0.175,
+		"answer": 0.025,
+		"answernr": 1
+	},
+	{
+		"type": "drawAsch",
+		"user": 2,
+		"v1": 0.9750000000000005,
+		"v2": 0.025,
+		"v3": 0.175,
+		"answer": 0.025,
+		"answernr": 1
+	},
+	{
+		"type": "drawAsch",
+		"user": 3,
+		"v1": 0.9750000000000005,
+		"v2": 0.025,
+		"v3": 0.175,
+		"answer": 0.025,
+		"answernr": 1
+	},
+	{
+		"type": "drawAsch",
+		"user": 4,
+		"v1": 0.9750000000000005,
+		"v2": 0.025,
+		"v3": 0.175,
+		"answer": 0.025,
+		"answernr": 1
+	},
+
+	{
+	  	type : "drawCommand",
+	  	v1 : "Lets start",
+	  	v2 : "Start now.",  	
+	},
+
+	// **************** Lines are hard ************
+	{
+		"type": "drawAsch",
+		"v1": 0.125,
+		"v2": 0.9750000000000005,
+		"v3": 0.9500000000000005,
+		"answer": 0.9500000000000005,
+		"answernr": 2,
+		"testSlide" : 79
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9500000000000005,
+		"v2": 0.9750000000000005,
+		"v3": 0.8500000000000004,
+		"answer": 0.9750000000000005,
+		"answernr": 1,
+		"testSlide" : 46
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9500000000000005,
+		"v2": 0.9750000000000005,
+		"v3": 0.15,
+		"answer": 0.9500000000000005,
+		"answernr": 0,
+		"testSlide" : 13
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9750000000000005,
+		"v2": 0.9500000000000005,
+		"v3": 0.175,
+		"answer": 0.9500000000000005,
+		"answernr": 1,
+		"testSlide" : 20
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9750000000000005,
+		"v2": 0.5250000000000001,
+		"v3": 0.9500000000000005,
+		"answer": 0.9750000000000005,
+		"answernr": 0,
+		"testSlide" : 38
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.7000000000000003,
+		"v2": 0.9500000000000005,
+		"v3": 0.9750000000000005,
+		"answer": 0.9500000000000005,
+		"answernr": 1,
+		"testSlide" : 61
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9500000000000005,
+		"v2": 0.8000000000000004,
+		"v3": 0.9750000000000005,
+		"answer": 0.9750000000000005,
+		"answernr": 2,
+		"testSlide" : 98
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9500000000000005,
+		"v2": 0.37500000000000006,
+		"v3": 0.9750000000000005,
+		"answer": 0.9500000000000005,
+		"answernr": 0,
+		"testSlide" : 31
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9750000000000005,
+		"v2": 0.7500000000000003,
+		"v3": 0.9500000000000005,
+		"answer": 0.9750000000000005,
+		"answernr": 0,
+		"testSlide" : 58
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9500000000000005,
+		"v2": 0.9750000000000005,
+		"v3": 0.8250000000000004,
+		"answer": 0.9500000000000005,
+		"answernr": 0,
+		"testSlide" : 64
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9750000000000005,
+		"v2": 0.9500000000000005,
+		"v3": 0.6000000000000002,
+		"answer": 0.9750000000000005,
+		"answernr": 0,
+		"testSlide" : 69
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9500000000000005,
+		"v2": 0.9000000000000005,
+		"v3": 0.9750000000000005,
+		"answer": 0.9500000000000005,
+		"answernr": 0,
+		"testSlide" : 77
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.4000000000000001,
+		"v2": 0.9750000000000005,
+		"v3": 0.9500000000000005,
+		"answer": 0.9750000000000005,
+		"answernr": 1,
+		"testSlide" : 100
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9500000000000005,
+		"v2": 0.3,
+		"v3": 0.9750000000000005,
+		"answer": 0.9500000000000005,
+		"answernr": 0,
+		"testSlide" : 105
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9250000000000005,
+		"v2": 0.9500000000000005,
+		"v3": 0.9750000000000005,
+		"answer": 0.9500000000000005,
+		"answernr": 1,
+		"testSlide" : 6
+	},
+
+
+	//**************** Lines are easy ************
+	{
+		"type": "drawAsch",
+		"v1": 0.5250000000000001,
+		"v2": 0.9750000000000005,
+		"v3": 0.025,
+		"answer": 0.5250000000000001,
+		"answernr": 0,
+		"testSlide" : 4
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.8000000000000004,
+		"v2": 0.9750000000000005,
+		"v3": 0.025,
+		"answer": 0.025,
+		"answernr": 2,
+		"testSlide" : 5
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.025,
+		"v2": 0.27499999999999997,
+		"v3": 0.05,
+		"answer": 0.27499999999999997,
+		"answernr": 1,
+		"testSlide" : 7
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.025,
+		"v2": 0.05,
+		"v3": 0.5250000000000001,
+		"answer": 0.5250000000000001,
+		"answernr": 2,
+		"testSlide" : 8
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9750000000000005,
+		"v2": 0.025,
+		"v3": 0.9500000000000005,
+		"answer": 0.025,
+		"answernr": 1,
+		"testSlide" : 10
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.4500000000000001,
+		"v2": 0.9750000000000005,
+		"v3": 0.9500000000000005,
+		"answer": 0.4500000000000001,
+		"answernr": 0,
+		"testSlide" : 12
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.05,
+		"v2": 0.125,
+		"v3": 0.025,
+		"answer": 0.125,
+		"answernr": 1,
+		"testSlide" : 14
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.9750000000000005,
+		"v2": 0.9500000000000005,
+		"v3": 0.7750000000000004,
+		"answer": 0.9500000000000005,
+		"answernr": 1,
+		"testSlide" : 16
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.05,
+		"v2": 0.025,
+		"v3": 0.22499999999999998,
+		"answer": 0.22499999999999998,
+		"answernr": 2,
+		"testSlide" : 18
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.22499999999999998,
+		"v2": 0.025,
+		"v3": 0.9750000000000005,
+		"answer": 0.22499999999999998,
+		"answernr": 0,
+		"testSlide" : 19
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.6000000000000002,
+		"v2": 0.9750000000000005,
+		"v3": 0.025,
+		"answer": 0.6000000000000002,
+		"answernr": 0,
+		"testSlide" : 21
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.025,
+		"v2": 0.15,
+		"v3": 0.05,
+		"answer": 0.05,
+		"answernr": 2,
+		"testSlide" : 23
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.6750000000000003,
+		"v2": 0.9750000000000005,
+		"v3": 0.9500000000000005,
+		"answer": 0.6750000000000003,
+		"answernr": 0,
+		"testSlide" : 24
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.07500000000000001,
+		"v2": 0.9500000000000005,
+		"v3": 0.9750000000000005,
+		"answer": 0.07500000000000001,
+		"answernr": 0,
+		"testSlide" : 25
+	},
+	{
+		"type": "drawAsch",
+		"v1": 0.37500000000000006,
+		"v2": 0.025,
+		"v3": 0.05,
+		"answer": 0.05,
+		"answernr": 2,
+		"testSlide" : 26
+	},
+	
+*/
+	{
+	  	type : "drawCommand",
+	  	v1 : "Vocal Experiment",
+	  	v2 : "Instructions and test round",  	
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["be", "have", "do", "speak", "mean"]
+	},
+
+
+	{
+	  	type : "drawCommand",
+	  	v1 : "Lets start",
+	  	v2 : "Start now.",  	
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["run", "spend", "lie", "fall", "pay"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["build", "catch", "cut", "grow", "buy"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["shake", "win", "teach", "lay", "throw"]
+	},
+
+	//**************** words are hard ************
+	
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["sing", "sink", "shrink", "spring", "ring"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["spill", "smell", "spoil", "spell", "learn"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["plead", "kneel", "leap", "dream", "lean"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["dive", "heave", "drive", "weave", "thrive"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["knit", "cost", "spit", "bust", "wet"]
+	},
+
+	//**************** words are hard ************
+	
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["say", "take", "find", "hear", "stand"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["make", "think", "leave", "bring", "lose"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["come", "know", "feel", "sit", "keep"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["go", "tell", "put", "hit", "read"]
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["get", "see", "give", "hold", "write"]
+	},
+
+]
