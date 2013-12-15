fd941f8b03eee21af71450c2d2db7da3c4435dd5
Juergen Brandstetter <juergen@tagaprice.org>
2013-12-15 17:21:10 +1300

    create word first gen

--- aschgen/public/wordFirstConfig.js
+++ aschgen/public/wordFirstConfig.js
@@ -18,38 +18,329 @@ window.configSlides =
 	{
 	  	type : "drawCommand",
 	  	v1 : "Introduction",
-	  	v2 : "Hello and welcome to our visual cognition and verbal communication test. Thank you for participating. The test consists of a visual task followed by a verbal task and takes about twenty minutes.",  	
-		sound : "instructions/inst1_hello.wav"
+	  	v2 : "Hello and welcome to our verbal communication and visual cognition test. Thank you for participating. The test consists of a verbal task followed by a visual task and takes about twenty minutes.",  	
+		sound : "instructions/inst_hello_lines_words.mp3"
 	},
 
 	{
 	  	type : "drawCommand",
 	  	v1 : "Introduction",
-	  	v2 : "The aim of this test is to assess the performance of the humanoid participants in visual cognition and verbal communication. Each of the humanoid participants has spent the last twelve months living with a New Zealand family and has used this time to train human-like cognitive abilities. The human participant is present in order to ensure that the test matches the Human Interface Technology Lab’s strict criteria for experimental design.",  	
-		sound : "instructions/inst2_intro_robot.wav"
+	  	v2 : "The aim of this test is to assess the performance of the five participants in visual cognition and verbal communication.",  	
+		sound : "instructions/inst_intro_human.mp3"
 	},
-/*
+
+
 	{
 	  	type : "drawCommand",
-	  	v1 : "Researcher is speaking",
-	  	v2 : "This test is used as a reference for another test, one that we are running separately. In that test, we are interested in the visual and verbal abilities of humanoid robots. In order to ensure that the test matches the human interface technology lab’s strict criteria for experimental design, we created a reference test which only has human participants. This way, we can compare the data later.",  	
-		sound : "instructions/inst2_intro_human.wav"
+	  	v1 : "Instructions Verbal Task",
+	  	v2 : "This task is a spoken production task. You will see verbs on the screen and you will have to say the verb and the past tense of the verb which belongs to you. Each verb belongs to one participant. So the first participant says the first verb and its  past tense, the second one says the second verb and its past tense, and so on.",
+	  	sound : "instructions/inst_verbal1.mp3"
 	},
-*/
+
+	{
+	  	type : "drawCommand",
+	  	v1 : "Instructions Verbal Task",
+	  	v2 : "Please remember to say the verb which is assigned to you, and the past tense of this verb. There are no good or bad answers here. Just say whatever form you would use in real life. And there is no reason to hurry.",
+	  	sound : "instructions/inst_verbal2.mp3"
+	},
+
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["like", "talk", "watch", "pick", "pull"],
+		sound : "instructions/inst_all_practice.mp3"
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["like", "talk", "watch", "pick", "pull"],
+		sound : "instructions/inst_verbal_pr_1.mp3"
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 1,
+		v1 : ["like", "talk", "watch", "pick", "pull"],
+		sound : "instructions/inst_verbal_pr_2.mp3"
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 2,
+		v1 : ["like", "talk", "watch", "pick", "pull"],
+		sound : "instructions/inst_verbal_pr_3.mp3"
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 3,
+		v1 : ["like", "talk", "watch", "pick", "pull"],
+		sound : "instructions/inst_verbal_pr_4.mp3"
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 4,
+		v1 : ["like", "talk", "watch", "pick", "pull"],
+		sound : "instructions/inst_verbal_pr_5.mp3"
+	},
+
+
+	{
+	  	type : "drawCommand",
+	  	v1 : "Verbal Task",
+	  	v2 : "Here comes the task. It consists of about thirty rounds.", 
+	  	sound : "instructions/inst_all_t.mp3"
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["look", "ask", "call", "seem", "use"],
+		robotW : "look",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 1,
+		v1 : ["look", "ask", "call", "seem", "use"],
+		robotW : "ask",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 2,
+		v1 : ["look", "ask", "call", "seem", "use"],
+		robotW : "call",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 3,
+		v1 : ["look", "ask", "call", "seem", "use"],
+		robotW : "seem",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 4,
+		v1 : ["look", "ask", "call", "seem", "use"],
+		robotW : "use",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["want", "turn", "change", "try", "start"],
+		robotW : "want",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 1,
+		v1 : ["want", "turn", "change", "try", "start"],
+		robotW : "turn",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 2,
+		v1 : ["want", "turn", "change", "try", "start"],
+		robotW : "change",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 3,
+		v1 : ["want", "turn", "change", "try", "start"],
+		robotW : "try",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 4,
+		v1 : ["want", "turn", "change", "try", "start"],
+		robotW : "start",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 0,
+		v1 : ["use", "walk", "stop", "work", "live"],
+		robotW : "use",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 1,
+		v1 : ["use", "walk", "stop", "work", "live"],
+		robotW : "walk",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 2,
+		v1 : ["use", "walk", "stop", "work", "live"],
+		robotW : "stop",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 3,
+		v1 : ["use", "walk", "stop", "work", "live"],
+		robotW : "work",
+		hardEasy : "cor",
+		cor : true
+	},
+
+	{
+		type : "drawManyWord",
+	  	user : 4,
+		v1 : ["use", "walk", "stop", "work", "live"],
+		robotW : "live",
+		hardEasy : "cor",
+		cor : true
+	},
+	
+	//**************** words are hard ************
+	
+	{
+		type : "finalRandMegaWords",
+		hardEasy : "hard",
+		cor : false,
+		iter: 3,
+		wordArray : [
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["sing", "sink", "shrink", "spring", "ring"],
+			scheme: "ung",
+		},
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["spill", "smell", "spoil", "spell", "learn"],
+			scheme: "nt",
+		},
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["plead", "kneel", "leap", "dream", "lean"],
+			scheme: "v",
+		},
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["dive", "heave", "drive", "weave", "thrive"],
+			scheme: "ove",
+		},
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["knit", "cost", "spit", "bust", "wet"],
+			scheme: "id",
+		},]
+	},
+
+	//**************** words are hard ************
+	{
+		type : "finalRandMegaWords",
+		hardEasy : "easy",
+		cor : false,
+		iter: 3,
+		wordArray : [
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["say", "take", "find", "hear", "stand"]
+		},
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["make", "think", "leave", "bring", "lose"]
+		},
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["come", "know", "feel", "sit", "keep"]
+		},
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["go", "tell", "put", "hit", "read"]
+		},
+
+		{
+			type : "drawManyWord",
+		  	user : 0,
+			v1 : ["get", "see", "give", "hold", "write"]
+		},]
+	},
+
+
+	{
+	  	type : "drawCommand",
+	  	v1 : "END of Verbal Test",
+	  	v2 : "Thank you for finishing this task.",
+	  	sound : "instructions/inst_cheers1.mp3"
+	},
 
 ////////////////////////////////////// FIRST TASK
 	{
 	  	type : "drawCommand",
 	  	v1 : "Introduction",
-	  	v2 : "The first task is a visual decision task. On the left hand side, you will see three bars, labelled A, B, and C. On the right hand side, you will see a reference bar. You will simply have to say the letter corresponding to the bar on the left that is the same height as the bar on the right.",  	
-		sound : "instructions/inst3_visual1.wav"
+	  	v2 : "This task is a visual decision task. On the left hand side, you will see three bars, labeled A, B, and C. On the right hand side, you will see a reference bar. You will simply have to say the letter corresponding to the bar on the left that is the same height as the bar on the right.",  	
+		sound : "instructions/inst_visual1.mp3"
 	},
 
 	{
 	  	type : "drawCommand",
 	  	v1 : "Introduction",
-	  	v2 : "To make it easier for us to evaluate the data afterwards, participants take turns from left to right, in the order in which they are seated. To make it easier for you to know when it’s your turn to say your answer, we have added five coloured squares on top of the screen. If the square with your colour is full, it is your turn to say the answer. If you are not sure about your colour or if you forgot it, you can refer to the sheet of paper of the matching colour which is in front of you.",  	
-		sound : "instructions/inst4_visual2.wav"
+	  	v2 : "To make it easier for us to evaluate the data afterwards, participants take turns from left to right, in the order in which they are seated. To make it easier for you to know when it’s your turn to say your answer, we have added five coloured squares on the screen. If the square with your colour is full, it is your turn to say the answer. If you are not sure about your colour or if you forgot it, you can refer to the sheet of paper of the matching colour which is in front of you.",  	
+		sound : "instructions/inst_firstblock.mp3"
+	},
+
+	{
+	  	type : "drawCommand",
+	  	v1 : "Introduction",
+	  	v2 : "Here is a practice round. All other rounds will look like this one.",  	
+		sound : "instructions/inst_all_practice.mp3"
 	},
 	
 
@@ -62,7 +353,7 @@ window.configSlides =
 		"answer": 0.9750000000000005,
 		"answernr": 0,
 		"loudRobot" : false,
-		sound : "instructions/inst5_visual_pr_0.wav"
+		sound : "instructions/inst_visual_pr_0.mp3"
 	},
 	{
 		"type": "drawAsch",
@@ -73,7 +364,7 @@ window.configSlides =
 		"answer": 0.9750000000000005,
 		"answernr": 0,
 		"loudRobot" : false,
-		sound : "instructions/inst6_visual_pr_1.wav"
+		sound : "instructions/inst_visual_pr_1.mp3"
 	},
 	{
 		"type": "drawAsch",
@@ -84,7 +375,7 @@ window.configSlides =
 		"answer": 0.9750000000000005,
 		"answernr": 0,
 		"loudRobot" : false,
-		sound : "instructions/inst7_visual_pr_2.wav"
+		sound : "instructions/inst_visual_pr_2.mp3"
 	},
 	{
 		"type": "drawAsch",
@@ -95,7 +386,7 @@ window.configSlides =
 		"answer": 0.9750000000000005,
 		"answernr": 0,
 		"loudRobot" : false,
-		sound : "instructions/inst8_visual_pr_3.wav"
+		sound : "instructions/inst_visual_pr_3.mp3"
 	},
 	{
 		"type": "drawAsch",
@@ -106,7 +397,7 @@ window.configSlides =
 		"answer": 0.9750000000000005,
 		"answernr": 0,
 		"loudRobot" : false,
-		sound : "instructions/inst9_visual_pr_4.wav"
+		sound : "instructions/inst_visual_pr_4.mp3"
 	},
 	{
 		"type": "drawAsch",
@@ -117,7 +408,7 @@ window.configSlides =
 		"answer": 0.9750000000000005,
 		"answernr": 0,
 		"loudRobot" : false,
-		sound : "instructions/inst10_visual_pr_5.wav"
+		sound : "instructions/inst_visual_pr_0.mp3"
 	},
 
 	{
@@ -603,289 +894,13 @@ window.configSlides =
 		},],
 	},
 	
-	{
-	  	type : "drawCommand",
-	  	v1 : "END of Visual Test",
-	  	v2 : "Thank you for finishing this task. Here comes the next task. It is a spoken production task. You will see verbs on the screen and you will have to say the verb and the past tense of the verb which belongs to you. Each verb belongs to one participant. So the first participant says the first verb and its  past tense, the second one says the second verb and its past tense, and so on.",
-	  	sound : "instructions/inst13_verbal1.wav"
-	},
-
-
-
-	{
-		type : "drawManyWord",
-	  	user : 0,
-		v1 : ["like", "talk", "watch", "pick", "pull"],
-		sound : "instructions/inst14_verbal_pr_0.wav"
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 0,
-		v1 : ["like", "talk", "watch", "pick", "pull"],
-		sound : "instructions/inst15_verbal_pr_1.wav"
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 1,
-		v1 : ["like", "talk", "watch", "pick", "pull"],
-		sound : "instructions/inst16_verbal_pr_2.wav"
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 2,
-		v1 : ["like", "talk", "watch", "pick", "pull"],
-		sound : "instructions/inst17_verbal_pr_3.wav"
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 3,
-		v1 : ["like", "talk", "watch", "pick", "pull"],
-		sound : "instructions/inst18_verbal_pr_4.wav"
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 4,
-		v1 : ["like", "talk", "watch", "pick", "pull"],
-		sound : "instructions/inst19_verbal_pr_5.wav"
-	},
-
-
-	{
-	  	type : "drawCommand",
-	  	v1 : "Verbal Task",
-	  	v2 : "Here comes the test. It again consists of about thirty rounds", 
-	  	sound : "instructions/inst20_visual_t.wav"
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 0,
-		v1 : ["look", "ask", "call", "seem", "use"],
-		robotW : "look",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 1,
-		v1 : ["look", "ask", "call", "seem", "use"],
-		robotW : "ask",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 2,
-		v1 : ["look", "ask", "call", "seem", "use"],
-		robotW : "call",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 3,
-		v1 : ["look", "ask", "call", "seem", "use"],
-		robotW : "seem",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 4,
-		v1 : ["look", "ask", "call", "seem", "use"],
-		robotW : "use",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 0,
-		v1 : ["want", "turn", "change", "try", "start"],
-		robotW : "want",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 1,
-		v1 : ["want", "turn", "change", "try", "start"],
-		robotW : "turn",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 2,
-		v1 : ["want", "turn", "change", "try", "start"],
-		robotW : "change",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 3,
-		v1 : ["want", "turn", "change", "try", "start"],
-		robotW : "try",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 4,
-		v1 : ["want", "turn", "change", "try", "start"],
-		robotW : "start",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 0,
-		v1 : ["use", "walk", "stop", "work", "live"],
-		robotW : "use",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 1,
-		v1 : ["use", "walk", "stop", "work", "live"],
-		robotW : "walk",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 2,
-		v1 : ["use", "walk", "stop", "work", "live"],
-		robotW : "stop",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 3,
-		v1 : ["use", "walk", "stop", "work", "live"],
-		robotW : "work",
-		hardEasy : "cor",
-		cor : true
-	},
-
-	{
-		type : "drawManyWord",
-	  	user : 4,
-		v1 : ["use", "walk", "stop", "work", "live"],
-		robotW : "live",
-		hardEasy : "cor",
-		cor : true
-	},
-	
-	//**************** words are hard ************
-	
-	{
-		type : "finalRandMegaWords",
-		hardEasy : "hard",
-		cor : false,
-		iter: 3,
-		wordArray : [
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["sing", "sink", "shrink", "spring", "ring"],
-			scheme: "ung",
-		},
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["spill", "smell", "spoil", "spell", "learn"],
-			scheme: "nt",
-		},
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["plead", "kneel", "leap", "dream", "lean"],
-			scheme: "v",
-		},
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["dive", "heave", "drive", "weave", "thrive"],
-			scheme: "ove",
-		},
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["knit", "cost", "spit", "bust", "wet"],
-			scheme: "id",
-		},]
-	},
-
-	//**************** words are hard ************
-	{
-		type : "finalRandMegaWords",
-		hardEasy : "easy",
-		cor : false,
-		iter: 3,
-		wordArray : [
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["say", "take", "find", "hear", "stand"]
-		},
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["make", "think", "leave", "bring", "lose"]
-		},
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["come", "know", "feel", "sit", "keep"]
-		},
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["go", "tell", "put", "hit", "read"]
-		},
-
-		{
-			type : "drawManyWord",
-		  	user : 0,
-			v1 : ["get", "see", "give", "hold", "write"]
-		},]
-	},
 
 	
 	{
 	  	type : "drawCommand",
 	  	v1 : "END of Experiment",
 	  	v2 : "The test is now complete. Thank you for participating. Please talk to the research assistant before you leave.",
-	  	sound : "instructions/inst21_tschuess.wav"
+	  	sound : "instructions/inst_cheers2.mp3"
 	}
 
 ]
