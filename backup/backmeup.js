1ef369e9f42be4e81623d606786b669af7acf046
Juergen Brandstetter <juergen@tagaprice.org>
2014-01-19 23:10:38 +1300

    now we know the word people where right or wrong

--- backup/backmeup.js
+++ backup/backmeup.js
@@ -2,13 +2,13 @@ var http = require('http');
 var fs = require('fs');
 var json = require('jsonreq');
 
-
+var globalWordCountList;
 
 function main (){
 	console.log("start");
-	pullResults(new Array(),0,100);
+	//pullResults(new Array(),0,100);
 	pullTest(new Array(),0,100);
-	pullExperresults(new Array(),23,100);
+	//pullExperresults(new Array(),23,100);
 
 };
 
@@ -58,6 +58,11 @@ function pullTest(container, skip, limit){
     		    		
     		storeFile("groupTestScripts.js", "var groupTestScripts="+JSON.stringify(container)+";");
             storeFile("groupTestScripts.json", JSON.stringify(container));
+
+
+            globalWordCountList = createNameCountObject(container);
+            //get experiment results
+            pullExperresults(new Array(),23,100);
     	}
 	});
 }
@@ -72,7 +77,10 @@ function pullExperresults(container, skip, limit){
     	if(data.length===100){
     		pullExperresults(container,skip+=100,100);
     	}else{
-    		//we reach this point only when we are finished    		    		
+    		//we reach this point only when we are finished    	
+        
+            container= addWordInfoToContainer(container);
+
     		storeFile("groupExperimentResults.js", "var groupExperimentResults="+JSON.stringify(container)+";");
             storeFile("groupExperimentResults.json", JSON.stringify(container));
     	}
@@ -91,6 +99,132 @@ function storeFile(filename, data){
 	});
 }
 
+function addWordInfoToContainer(container){
+    //globalWordCountList
+
+    //console.log(globalWordCountList);
+    for(a in container){
+        console.log("part",container[a]["res"]["part"], globalWordCountList[container[a]["res"]["part"]]);
+
+        var _locCount = globalWordCountList[container[a]["res"]["part"]];
+
+        //word abig
+        
+        for(k in container[a]["res"]["wordsAmbig"]){
+            //container[a]["res"]["wordsAmbig"][k]["id"];
+
+            for(c in _locCount){
+                if(_locCount[c]["id"] == container[a]["res"]["wordsAmbig"][k]["id"]){
+                    container[a]["res"]["wordsAmbig"][k]["word"] = _locCount[c]["word"];
+                    container[a]["res"]["wordsAmbig"][k]["hearCount"] = _locCount[c]["hearCount"];
+                }
+            }
+        }
+
+        //word nonAmbig
+        for(k in container[a]["res"]["wordsNAmbig"]){
+            //container[a]["res"]["wordsAmbig"][k]["id"];
+
+            for(c in _locCount){
+                if(_locCount[c]["id"] == container[a]["res"]["wordsNAmbig"][k]["id"]){
+                    container[a]["res"]["wordsNAmbig"][k]["word"] = _locCount[c]["word"];
+                    container[a]["res"]["wordsNAmbig"][k]["hearCount"] = _locCount[c]["hearCount"];
+                }
+            }
+        }
+    }
+
+    return container;
+}
+
+
+function createNameCountObject(container){
+
+    var nameObject = {};
+    //nameObject['153'] = "15";
+    
+    for(a in container){
+        //console.log(parseInt(container[a]["name"])+100);
+        var _store = wordRepConf(container[a]["everything"]);
+        nameObject[container[a]["name"]] = _store;
+        nameObject[parseInt(container[a]["name"])+100] = _store;
+    }
+
+
+    return nameObject;
+
+}
+
+
+function wordRepConf (argument) {
+    // body...
+
+
+var md = argument;
+//md = dataWordsFirst;
+
+var offset = 28;
+var lowset = 177;
+
+if(md[12].v1=="Verbal Task"){
+    //words first
+    //console.log("Verbal Task", md[12]);
+}else if(md[12].v1=="Visual test"){
+    //lines first
+    //console.log("Visual test", md[12]);
+    offset=203;
+    lowset=352;
+}else{
+    //console.log("ubsi", md[12]);
+}
+
+
+//console.log('----------------------------');
+
+
+
+//console.log(md[offset]);
+//console.log(md[lowset]);
+md=md.splice(offset, 150);
+//console.log(md[0]);
+//console.log(md[149]);
+
+var heardWord = [];
+
+for(var i=md.length-1; i>=0; i=i-5){
+    //console.log(i, md[i]["v1"][4]);
+
+    //loop through rest
+    var size = "";
+    var bla = 0;
+    var howOften = 0;
+    for(var k=i-5; k>=0; k=k-5){
+        //console.log(i, md[i]["v1"][4], md[k]["v1"][4]);
+        size+="-";
+        bla++;
+
+        if(md[i]["v1"][4]==(md[k]["v1"][3] || md[k]["v1"][2] || md[k]["v1"][1] || md[k]["v1"][0])){
+            //console.log("found");
+            howOften++;
+        }
+    }
+
+    //console.log(md[i]["v1"][4], howOften, i+offset);
+
+    heardWord.push({
+        "id" : i+offset,
+        "word" : md[i]["v1"][4],
+        "hearCount" : howOften
+    });
+
+    //console.log(bla, size);
+}
+
+//console.log(heardWord);
+
+return heardWord;
+
+}
 
 
 main();
